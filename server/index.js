4cf889e30fca86afdba12c540b3b7c9ac5d2e275
Elliot Palominos <elliot.palominos@externos.bci.cl>
2018-08-01 23:37:41 -0400

    Morgan is installed for requests and the function to interpret JSON is added

--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
+const morgan = require('morgan');
 const app = express();
 
 // Settings
-app.set('port', 3000)
+app.set('port', 3000);
 
 // Middlewares
+app.use(morgan('dev'));
+app.use(express.json());
 
 // Routes
 
 // Starting the server
 app.listen(app.get('port'), () => {
-    console.log('Server on port 3000');
-})
+    console.log('Server on port', app.get('port'));
+});
