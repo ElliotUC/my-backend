531ad7237b8c38e5501cc637869a3037359328bc
ElliotUC <elliot.palominos@gmail.com>
2018-08-02 01:43:31 -0400

    Revert "Feature get api albums (#3)"
    This reverts commit edc762959b89195b10162529d169f38626d0558a.

--- server/database.js
+++
@@ -1,9 +0,0 @@
-const moongose = require('mongoose');
-
-const URI = 'mongodb://localhost/spotify-albums';
-
-moongose.connect(URI)
-    .then(db => console.log('DB is ready'))
-    .catch(err => console.error(err));
-
-module.exports = moongose;
--- server/routes/spotify.routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-
-const searchAlbumController = require('../controllers/searchAlbum.controller');
-
-router.get('/', searchAlbumController.getAlbumBySearch);
-
-module.exports = router;
