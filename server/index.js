519058578a0beb65746f2d4f98b105291aac2524
Elliot Palominos <elliot.palominos@externos.bci.cl>
2018-08-02 00:14:47 -0400

    Route for services is added

--- server/index.js
+++ server/index.js
@@ -12,6 +12,7 @@ app.use(morgan('dev'));
 app.use(express.json());
 
 // Routes
+app.use('/api/albumBySearch', require('./routes/spotify.routes'));
 
 // Starting the server
 app.listen(app.get('port'), () => {
